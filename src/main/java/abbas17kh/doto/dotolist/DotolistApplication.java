82b663d84d247a586b028ca309955f2255c687f6
abbas17kh <halilabbs97@gmail.com>
2025-09-03 20:14:00 +0200

    init

---
+++ src/main/java/abbas17kh/doto/dotolist/DotolistApplication.java
@@ -0,0 +1,13 @@
+package abbas17kh.doto.dotolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotolistApplication.class, args);
+	}
+
+}
